4423d227d5de8a4b8d0739483fb4e00af0b7c930
MidnightSkyUniverse <midnightskyuniverse@yahoo.com>
2021-11-21 16:21:04 +0100

    Tests for all functions and twicks to churn_library

--- churn_library.py
+++ churn_library.py
@@ -1,89 +1,375 @@
 '''
-    Project: 'Customer Churn Project' for Udacity nanodegree program
+    Classes for Churn project
 
     Date: Nov 2021
 
     Author: Ali Binkowska
 '''
 # import libraries
-#from sklearn.model_selection import train_test_split
-#from sklearn.linear_model import LogisticRegression
-#from sklearn.ensemble import RandomForestClassifier
-#from sklearn.model_selection import GridSearchCV
-#from sklearn.metrics import plot_roc_curve, classification_report
-#import shap
-#import joblib
-#import pandas as pd
-#import numpy as np
-#import matplotlib.pyplot as plt
-#import seaborn as sns
-# Import pcfg values
-import projectconfig as pcfg
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import plot_roc_curve, classification_report
+from sklearn.preprocessing import OneHotEncoder
+import shap
+import joblib
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 # Initiate seaborn theme
-#sns.set()
-import churn_classes as cls
+sns.set()
 
+
+class TrainModel():
+    '''
+
+    This class has three methods: fit, predict and save_model
+    Model classes for Logistic Regression and Random Forest Classifier inherits from TrainModel
+
+    '''
+
+    def __init__(self):
+        self.model = None
+
+    def fit(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X_train, X_test, is_best_estimator):
+        '''
+        X: data
+        y: target
+
+        '''
+        if is_best_estimator:
+            y_train_preds = self.model.best_estimator_.predict(X_train)
+            y_test_preds = self.model.best_estimator_.predict(X_test)
+        else:
+            y_train_preds = self.model.predict(X_train)
+            y_test_preds = self.model.predict(X_test)
+
+        return (y_train_preds, y_test_preds)
+
+    def save_model(self, pth):
+        '''
+             saves model to ./models as .pkl file
+                input:
+                    model: model object
+                    output_pth: path to store the model
+                output:
+                    None
+        '''
+        joblib.dump(self.model, pth)
+
+    def load_model(self,pth):
+        '''
+            get model from ./models as .pkl file
+                input:
+                    model: model object
+                    output_pth: path to store the model
+                output:
+                    None
+        '''
+        self.model = joblib.load(pth)
+
+
+class MyLogisticRegression(TrainModel):
+    '''
+
+        Class creates LogisticRegresion model object
+
+    '''
+
+    def __init__(self, random_state=None, max_iter=100):
+        super().__init__()
+        self.model = LogisticRegression(
+            random_state=random_state, max_iter=max_iter)
+        self.name = 'Logistic Regression'
+
+
+class MyRandomForestClassifier(TrainModel):
+    '''
+
+        Class creates Random Forest Classifier model object
+        It has two methods to define best_estimator_ and feature_importances_
+
+    '''
+
+    def __init__(self, param_grid, random_state=42, cv=5):
+        super().__init__()
+        self.rfc = RandomForestClassifier(random_state=random_state)
+        self.model = GridSearchCV(
+            estimator=self.rfc,
+            param_grid=param_grid,
+            cv=cv)
+        self.name = 'Random Forest Classifier'
+
+
+class DataEncoding():
+    '''
+    '''
+
+    def __init__(self):
+        self.data = pd.DataFrame()
+        self.X = pd.DataFrame()
+        self.y = None
+
+    def import_data(self, pth):
+        '''
+        returns dataframe for the csv found at pth
+        input:
+            pth: a path to the csv
+        output:
+            df: pandas dataframe
+        '''
+
+        self.data = pd.read_csv(pth)
+
+    def clean_data(self, new_col='Churn'):
+        '''
+        add column Churn that encodes column Existing Customer with 0 and 1
+            input:
+                df: pandas dataframe
+            output:
+                df: pandas dataframe
+        '''
+        self.data[new_col] = self.data['Attrition_Flag'].apply(
+            lambda val: 0 if val == "Existing Customer" else 1)
+
+        labels_to_drop = [
+            'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1',
+            'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2',
+        ]
+        self.data.drop(labels=labels_to_drop, axis=1, inplace=True)
+
+    def encoder_helper(self, category_lst, keep_cols, response):
+        '''
+        helper function to turn each categorical column into a new column with
+        propotion of churn for each category - associated with cell 15 from the notebook
+
+        input:
+            df: pandas dataframe
+            category_lst: list of columns that contain categorical features
+            response: string of response name [optional argument that could be used
+                    for naming variables or index y column]
+
+        output:
+            df: pandas dataframe with new columns for
+        '''
+        for col in category_lst:
+            col_lst = []
+            col_groups = self.data.groupby(col).mean()[response]
+
+            for val in self.data[col]:
+                col_lst.append(col_groups.loc[val])
+            new_col = col + '_' + response
+            self.data[new_col] = col_lst
+
+        self.X[keep_cols] = self.data[keep_cols]
+        self.y = self.data[response]
+
+    def one_hot_encoder(self, data, category_lst, response):
+        '''
+        helper function to turn each categorical column into a new column with one-hot encoder
+
+        input:
+            df: pandas dataframe
+            category_lst: list of columns that contain categorical features
+            response: string of response name [optional argument that could be used
+                    for naming variables or index y column]
+
+        output:
+            df: pandas dataframe with new columns for
+        '''
+        enc = OneHotEncoder(handle_unknown='ignore', sparse=False)
+        cols_encoded = pd.DataFrame(enc.fit_transform(data[category_lst]))
+        cols_remaining = data.drop(category_lst, axis=1)
+
+        self.X = pd.concat([cols_encoded, cols_remaining], axis=1)
+        self.y = data[response]
+
+
+class FeatureEngineering():
+    '''
+        input:
+              df: pandas dataframe
+              response: string of response name [optional argument that could be used
+                        for naming variables or index y column]
+
+        output:
+              X_train: X training data
+              X_test: X testing data
+              y_train: y training data
+              y_test: y testing data
+    '''
+
+    def __init__(self):
+        #    '''
+        #    '''
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
+
+    def engineering(self, X_data, y_data, test_size=0.3, random_state=42):
+        '''
+
+            Function splts data into train and test data
+
+            input:
+                X_data, y_data
+                test_size: how much data we want in test sample
+                random_state: how much andomized the data can be
+            output:
+                X_train, y_train, X_test, y_test
+        '''
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            X_data, y_data, test_size=test_size, random_state=random_state)
+
+    def function_placeholder(self):
+        pass
+
+
+class MyFigure(plt.Figure):
+    '''
+    perform eda on df and save figures to images folder
+    input:
+            df: pandas dataframe
+
+    output:
+            None
+    '''
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fig = plt
+        self.fig.figure(figsize=kwargs['figsize'])
+
+    def save_figure(self, pth):
+        self.fig.savefig(pth, bbox_inches='tight')
+        self.fig.clf()
+
+    def plot_histogram(self, data, col, title, pth):
+
+        data[col].hist()
+        self.fig.title(title)
+        self.save_figure(pth)
+
+    def plot_chart(self, data, col, title, pth, chart_type='bar'):
+
+        data[col].value_counts('normalize').plot(kind=chart_type)
+        self.fig.title(title)
+        self.save_figure(pth)
+
+    def plot_distribution_chart(self, data, col, title, pth):
+
+        sns.displot(data[col])
+        self.fig.title(title)
+        self.save_figure(pth)
+
+    def plot_heatmap(
+            self,
+            data,
+            title,
+            pth,
+            cmap='Dark2_r',
+            annot=False,
+            linewidths=2):
+        sns.heatmap(data.corr(), annot=annot, cmap=cmap, linewidths=linewidths)
+        self.fig.title(title)
+        self.save_figure(pth)
+
+    def feature_importances(self, model, X_data, pth):
+        '''
+        creates and stores the feature importances in pth
+        input:
+            model: model object containing feature_importances_
+            X_data: pandas dataframe of X values
+            output_pth: path to store the figure
+
+        output:
+             None
+        '''
+        # Calculate feature importances
+        importances = model.best_estimator_.feature_importances_
+        # Sort feature importances in descending order
+        indices = np.argsort(importances)[::-1]
+
+        # Rearrange feature names so they match the sorted feature importances
+        names = [X_data.columns[i] for i in indices]
+
+        # Create plot title
+        self.fig.title("Feature Importance")
+        self.fig.ylabel('Importance')
+
+        # Add bars
+        self.fig.bar(range(X_data.shape[1]), importances[indices])
+
+        # Add feature names as x-axis labels
+        self.fig.xticks(range(X_data.shape[1]), names, rotation=90)
+
+        # Save figure
+        self.save_figure(pth)
+
+    def classification_report(self, model_name, y_train,
+                              y_test,
+                              y_train_preds,
+                              y_test_preds,
+                              pth):
+        '''
+        produces classification report for training and testing results and stores report as image
+        in images folder
+        input:
+            y_train: training response values
+            y_test:  test response values
+            y_train_preds: training predictions from a model
+            y_test_preds: test predictions from logistic regression
+            model_name: name of a model
+            image_name: name of image to store as .png file
+        output:
+             None
+        '''
+        #plt.rc('figure', figsize=(5, 5))
+        self.fig.text(0.01, 0.95, str(f'{model_name} Train'), {
+            'fontsize': 10}, fontproperties='monospace')
+        self.fig.text(0.01, 0.55, str(classification_report(y_train, y_train_preds)), {
+            'fontsize': 10}, fontproperties='monospace')  # approach improved by OP -> monospace!
+        self.fig.text(0.01, 0.45, str(f'{model_name} Test'), {
+            'fontsize': 10}, fontproperties='monospace')
+        self.fig.text(0.01, 0.01, str(classification_report(y_test, y_test_preds)), {
+            'fontsize': 10}, fontproperties='monospace')  # approach improved by OP -> monospace!
+        self.fig.axis('off')
+        self.save_figure(pth)
+
+    def roc_curve_plot(self, model_1, model_2, X_test, y_test, pth):
+        '''
+        creates and stores the feature importances in pth
+        input:
+            model: model object containing feature_importances_
+            X_data: pandas dataframe of X values
+            output_pth: path to store the figure
+
+        output:
+             None
+        '''
+        ax = plt.gca()
+        lrc_plot = plot_roc_curve(model_1, X_test, y_test, ax=ax, alpha=0.8)
+        lrc_plot = plot_roc_curve(model_2, X_test, y_test, ax=ax, alpha=0.8)
+        self.save_figure(pth)
+
+    def explainer_plot(self, model, X_test, plot_type, pth):
+        '''
+
+        This chart uses shap to add explainer
+
+        '''
+        explainer = shap.TreeExplainer(model.best_estimator_)
+        shap_values = explainer.shap_values(X_test)
+        shap.summary_plot(shap_values, X_test, plot_type=plot_type, show=False)
+        self.fig.title("Explainer bar chart")
+        self.save_figure(pth)
 
 
 if __name__ == '__main__':
-    '''
-        comman line tests of churn_classes
-    '''
-    ml_data = cls.DataEncoding()
-    ml_data.import_data(pcfg.data_file_path)
-    ml_data.clean_data('Churn')
-    
-    eda = [cls.MyFigure(figsize=(15,8)) for i in range(5)]
-    eda[0].plot_histogram(ml_data.data, 'Churn', 'Churn histogram',pcfg.churn_distribution)
-    eda[1].plot_histogram(ml_data.data, 'Customer_Age', "Histogram - Customer Age" ,pcfg.customer_age_distribution)
-    eda[2].plot_chart(ml_data.data, 'Marital_Status', "Bar chart - Marital status", pcfg.marital_status_distribution, 'bar')
-    eda[3].plot_distribution_chart(ml_data.data, 'Total_Trans_Ct', "Distribution chart - Total transactions", pcfg.total_transation_distribution)
-    eda[4].plot_heatmap(ml_data.data, 'Heatmap', pcfg.heatmap )#annot=False, cmap='Dark2_r', linewidths = 2, pth=pcfg.heatmap)
-        
-
-    encoded_data = cls.DataEncoding()
-    encoded_data.encoder_helper(ml_data.data, pcfg.cat_columns, pcfg.keep_columns, 'Churn')
-    featured_data = cls.FeatureEngineering()
-    featured_data.engineering(encoded_data.X,encoded_data.y)
-        
-    model_lr = cls.MyLogisticRegression()
-    model_lr.fit(featured_data.X_train, featured_data.y_train)    
-    y_train_preds, y_test_preds = model_lr.predict(featured_data.X_train, featured_data.X_test, best_estimator=False)
-    model_lr.save_model(pcfg.lr_model)
-
-    
-    classification_figure_lr = cls.MyFigure(figsize=(15,8))
-    classification_figure_lr.classification_report(model_lr.name, featured_data.y_train, featured_data.y_test, y_train_preds, y_test_preds,pcfg.logistic_results)   
-    
-
-    param_grid = {
-        'n_estimators': [200, 500],
-        'max_features': ['auto', 'sqrt'],
-        'max_depth': [4, 5, 100],
-        'criterion': ['gini', 'entropy']
-    }
-
-    # Declare model
-    model_rfc =  cls.MyRandomForestClassifier(param_grid)
-    # Fit model
-    model_rfc.fit(featured_data.X_train, featured_data.y_train)
-    # Predict model
-    y_train_preds, y_test_preds = model_rfc.predict(featured_data.X_train, featured_data.X_test, best_estimator=True)
-    model_rfc.save_model(pcfg.rfc_model)
-     
-    classification_figure_rfc = cls.MyFigure(figsize=(15,8))
-    classification_figure_rfc.classification_report(model_rfc.name, featured_data.y_train, featured_data.y_test, y_train_preds, y_test_preds,pcfg.rfc_results)   
-
-
-    # Feature importances plot
-    importances_plot = cls.MyFigure(figsize=(15,8))
-    importances_plot.feature_importances(model_rfc.model, encoded_data.X, pcfg.feature_importances)
-    
-    roc = cls.MyFigure(figsize=(15,8))
-    roc.roc_curve_plot(model_lr.model, model_rfc.model.best_estimator_, featured_data.X_test, featured_data.y_test, pcfg.roc_curve_result)
-
-    expl = cls.MyFigure(figsize=(15,8))
-    expl.explainer_plot(model_rfc.model, featured_data.X_test, "bar", pcfg.explainer)
-
-    
-
+    print('Churn classes')
--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -1,4 +1,3 @@
-import os
 import logging
 
 # Project config and classes
@@ -11,13 +10,14 @@ logging.basicConfig(
     level=logging.INFO,
     filemode='w',
     force=True,
-    format='%(name)s - %(levelname)s - %(message)s')
+    format='%(asctime)s: %(levelname)s - %(message)s')
 
 
 def test_building_model():
     '''
     Test of class DataEncoding()
     '''
+    # Test of data extraction and cleaning
     try:
         data = cls.DataEncoding()
         isinstance(data, cls.DataEncoding)
@@ -28,9 +28,9 @@ def test_building_model():
 
     try:
         data.import_data(cfg.data_file_path)
-        logging.info("Testing import_data: SUCCESS")
+        logging.info("Method import_data: SUCCESS")
     except AssertionError as err:
-        logging.info("Testing import_data - file not found: FAILED")
+        logging.info("Method import_data - file not found: FAILED")
         raise err
 
     try:
@@ -38,21 +38,24 @@ def test_building_model():
         assert data.data.shape[1] > 0
         logging.info("Testing shape of the imported data: SUCCESS")
     except AssertionError as err:
-        logging.error("Testing import_data: The file doesn't appear to have rows and columns")
+        logging.error(
+            "Testing import_data: The file doesn't appear to have rows and columns")
         raise err
-    
+
     try:
         data.clean_data('Churn')
-        logging.info("Testing clean_data method: SUCCESS")
+        logging.info("Method clean_data method: SUCCESS")
     except AssertionError as err:
-        logging.error("Testing clean_data: issue during adding a new column and dropping two others")
+        logging.error(
+            "Method clean_data: issue during adding a new column and dropping two others")
         raise err
-    
+
+    # Data prepping process
     try:
         data.encoder_helper(cfg.cat_columns, cfg.keep_columns, 'Churn')
-        logging.info("Testing encoder_helper method: SUCCESS")
+        logging.info("Method encoder_helper method: SUCCESS")
     except AssertionError as err:
-        logging.error("Testing encoder_helper: issue with setting X and y")
+        logging.error("Method encoder_helper: issue with setting X and y")
         raise err
 
     try:
@@ -65,11 +68,13 @@ def test_building_model():
 
     try:
         featured.engineering(data.X, data.y)
-        logging.info("Testing engineering method: SUCCESS")
+        logging.info("Method engineering method: SUCCESS")
     except AssertionError as err:
-        logging.error("Testing clean_data: issue during adding a new column and dropping two others")
+        logging.error(
+            "Method clean_data: issue during adding a new column and dropping two others")
         raise err
 
+    # Test of Logistic Regression model creation
     try:
         model_lr = cls.MyLogisticRegression()
         isinstance(model_lr, cls.MyLogisticRegression)
@@ -78,15 +83,34 @@ def test_building_model():
         logging.error("Object MyLogisticRegression() was not created:")
         raise err
 
+    # Test of Logistic Regression model fitting
     try:
-        featured.engineering(data.X, data.y)
-        logging.info("Testing engineering method: SUCCESS")
+        model_lr.fit(featured.X_train, featured.y_train)
+        logging.info("Method Logistic Regression fit method: SUCCESS")
     except AssertionError as err:
-        logging.error("Testing clean_data: issue during adding a new column and dropping two others")
+        logging.error("Method Logistic Regression fit: the process has failed")
         raise err
 
+    # Test of Logistic Regression model predicting
+    try:
+        y_train_preds_lr, y_test_preds_lr = model_lr.predict(
+            featured.X_train, featured.X_test, is_best_estimator=False)
+        logging.info("Method Logistic Regression predict method: SUCCESS")
+    except AssertionError as err:
+        logging.error(
+            "Method Logistic Regression predict: the process has failed")
+        raise err
 
-        
+     # Test of Logistic Regression model saving
+    try:
+        model_lr.save_model(cfg.lr_model)
+        logging.info("Method Logistic Regression save method: SUCCESS")
+    except AssertionError as err:
+        logging.error(
+            "Method Logistic Regression save: the process has failed")
+        raise err
+
+    # Test of Random Forest Classifier model creation
     try:
         param_grid = {
             'n_estimators': [200, 500],
@@ -102,10 +126,169 @@ def test_building_model():
         logging.error("Object MyRandomForestClassifier() was not created:")
         raise err
 
+    # Test of Random Forest Classifier model fitting
+    try:
+        model_rfc.fit(featured.X_train, featured.y_train)
+        logging.info("Method Random Forest Classifier fit method: SUCCESS")
+    except AssertionError as err:
+        logging.error(
+            "Method Random Forest Classifier fit: the process has failed")
+        raise err
+
+    # Test of Random Forest Classifier model predicting
+    try:
+        y_train_preds_rfc, y_test_preds_rfc = model_rfc.predict(
+            featured.X_train, featured.X_test, is_best_estimator=True)
+        logging.info("Method Random Forest Classifier predict method: SUCCESS")
+    except AssertionError as err:
+        logging.error(
+            "Method Random Forest Classifier predict: the process has failed")
+        raise err
+
+    # Model saving
+    try:
+        model_rfc.save_model(cfg.rfc_model)
+        logging.info("Method Random Forest Classifier save method: SUCCESS")
+    except AssertionError as err:
+        logging.error(
+            "Method Random Forest Classifier save: the process has failed")
+        raise err
+
+    try:
+        model_lr.save_model(cfg.lr_model)
+        logging.info("Method Logistic Regression save method: SUCCESS")
+    except AssertionError as err:
+        logging.error(
+            "Method Logistic Regression save: the process has failed")
+        raise err
+
+    # Model loading
+    try:
+        model_lr = cls.MyLogisticRegression()
+        model_lr.load_model(cfg.lr_model)
+        logging.info("Method Logistic Regression save method: SUCCESS")
+    except AssertionError as err:
+        logging.error(
+            "Method Logistic Regression save: the process has failed")
+        raise err
+
+    try:
+        param_grid = {}
+        model_rfc = cls.MyRandomForestClassifier(param_grid)
+        model_rfc.load_model(cfg.rfc_model)
+        logging.info("Method Random Forest Classifier save method: SUCCESS")
+    except AssertionError as err:
+        logging.error(
+            "Method Random Forest Classifier save: the process has failed")
+        raise err
+
+    # Tests for reports and charts
+    try:
+        eda = cls.MyFigure(figsize=(15, 8))
+        eda.plot_histogram(
+            data.data,
+            'Churn',
+            'Churn histogram',
+            cfg.churn_distribution)
+        eda.clf()
+        logging.info("Figure plot_histogram: SUCCESS")
+    except AssertionError as err:
+        logging.error("Figure plot_histogram: the process has failed")
+        raise err
+
+    try:
+        eda = cls.MyFigure(figsize=(15, 8))
+        eda.plot_chart(
+            data.data,
+            'Marital_Status',
+            "Bar chart - Marital status",
+            cfg.marital_status_distribution,
+            'bar')
+        eda.clf()
+        logging.info("Figure plot_chart: SUCCESS")
+    except AssertionError as err:
+        logging.error("Figure plot_chart: the process has failed")
+        raise err
+
+    try:
+        eda = cls.MyFigure(figsize=(15, 8))
+        eda.plot_distribution_chart(
+            data.data,
+            'Total_Trans_Ct',
+            "Distribution chart - Total transactions",
+            cfg.total_transation_distribution)
+        eda.clf()
+        logging.info("Figure plot_distribution_chart: SUCCESS")
+    except AssertionError as err:
+        logging.error("Figure plot_distribution_chart: the process has failed")
+        raise err
+
+    try:
+        eda = cls.MyFigure(figsize=(15, 8))
+        eda.plot_heatmap(data.data, 'Heatmap', cfg.heatmap)
+        eda.clf()
+        logging.info("Figure plot_heatmap: SUCCESS")
+    except AssertionError as err:
+        logging.error("Figure plot_heatmap: the process has failed")
+        raise err
+
+    try:
+        fig = cls.MyFigure(figsize=(15, 8))
+        fig.classification_report(
+            model_lr.name,
+            featured.y_train,
+            featured.y_test,
+            y_train_preds_lr,
+            y_test_preds_lr,
+            cfg.logistic_results)
+        eda.clf()
+        logging.info("Figure classification_report: SUCCESS")
+    except AssertionError as err:
+        logging.error("Figure classification_report: the process has failed")
+        raise err
+
+    try:
+        eda = cls.MyFigure(figsize=(15, 8))
+        eda.feature_importances(
+            model_rfc.model,
+            data.X,
+            cfg.feature_importances)
+        eda.clf()
+        logging.info("Figure feature_importances: SUCCESS")
+    except AssertionError as err:
+        logging.error("Figure feature_importances: the process has failed")
+        raise err
+
+    try:
+        eda = cls.MyFigure(figsize=(15, 8))
+        eda.roc_curve_plot(
+            model_lr.model,
+            model_rfc.model.best_estimator_,
+            featured.X_test,
+            featured.y_test,
+            cfg.roc_curve_result)
+        eda.clf()
+        logging.info("Figure roc_curve_plot: SUCCESS")
+    except AssertionError as err:
+        logging.error("Figure roc_curve_plot: the process has failed")
+        raise err
+
+    try:
+        eda = cls.MyFigure(figsize=(15, 8))
+        eda.explainer_plot(
+            model_rfc.model,
+            featured.X_test,
+            "bar",
+            cfg.explainer)
+        eda.clf()
+        logging.info("Figure explainer_plot: SUCCESS")
+    except AssertionError as err:
+        logging.error("Figure explainer_plot: the process has failed")
+        raise err
+
 
 if __name__ == "__main__":
     pass
     # test_eda(perform_eda):
     # test_perform_feature_engineering(perform_feature_engineering):
     # test_train_models(train_models):
-
